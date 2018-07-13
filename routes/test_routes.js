c67eded55989808fdcdabeee3c59f2460b1fa63f
Jimmy Guo <jimmyzidanguo@hotmail.com>
2018-07-13 12:38:24 -0400

    asd

--- routes/test_routes.js
+++ routes/test_routes.js
@@ -1,3 +1,4 @@
+const generateInitialEmail = require('../api/ses_api').generateInitialEmail
 
 // GET /test
 exports.test = function(req, res, next){
@@ -11,4 +12,17 @@ exports.auth_test = function(req, res, next){
   res.json({
     message: "Auth test says alive and well"
   })
+}
+
+exports.email_test = function(req, res, next){
+  generateInitialEmail('support@renthero.com', 'test corp')
+    .then((data) => {
+      console.log(data)
+      res.json({
+        message: 'Email test success'
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
